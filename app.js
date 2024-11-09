06ea6c0f91b378ff286f05e8b2cd7941b47e92ba
Tatiana Ciocca Almeida <18190041+Tacial@users.noreply.github.com>
2024-11-08 22:57:28 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+alert('Boas vindas ao jogo do Número Secreto!');
+let numeroMaximo = 100
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+let chute; 
+let tentativa = 1;
+
+// enquanto comentário de 1 linha
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+
+
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto){
+            alert(`o número secreto é menor que ${chute}`);
+        } else {
+            alert(`o número secreto é maior que ${chute}`);
+        }
+    }
+    tentativa ++;
+}
+
+let palavraTentativa = tentativa > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativa} ${palavraTentativa}.`);
+
+//if (tentativa > 1) {
+//    alert(`Você acertou depois de ${tentativa} tentativas`);
+//} else {
+//    alert(`Você acertou com ${tentativa} tentativa`);
+//}
